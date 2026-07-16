8532240f0b17b5f4cd9c34f190ca7ed64cbc096f
Pranjul Gupta <pranjulguptaknp@gmail.com>
2026-07-17 02:45:57 +0530

    Python 3
    1.Strings
    2. variable checking

---
+++ String.py
@@ -0,0 +1,20 @@
+# Diffrent ways to create a string
+str1 = "This is a string"
+str2 ='This is also a string'
+str3 = """This is also a string"""
+
+print(str1)
+
+# How to add a string
+final_string = str1+str2
+print(final_string)
+
+# How to check the length of a string
+a = len(str1)
+print(a)
+
+# How to print the index value of a string(Each data is stored in form of index from 0 to infinity)
+
+a = "PranjulGupta"
+print(a[6])
+# it will print n as n comes in 3 position from starting
---
+++ Var check.py
@@ -0,0 +1,13 @@
+# How to check a variable datatype
+a = 5
+t = type(a)
+print(t)
+
+c = 31.2
+e = type(c)
+print(e)
+
+# if a data(no.) is given in quotes then also its string
+b = "100"
+f = type(b)
+print(f)
